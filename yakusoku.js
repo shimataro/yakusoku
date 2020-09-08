7d662523c2f9e406cebfff0a49420ee15abb8067
shimataro <shimataro@zelkova.cc>
2020-09-09 06:34:57 +0900

    コンストラクターを作成

--- yakusoku.js
+++ yakusoku.js
@@ -1,5 +1,30 @@
 class Yakusoku {
-  constructor() {
+  constructor(func) {
     this.state = "pending"; // 内部状態; pending / fulfilled / rejected
+    this.resolvedValue = null; // resolve()で渡された値を保持
+    this.rejectedValue = null; // reject()で渡された値を保持
+
+    const resolve = (resolvedValue) => {
+      if (this.state !== "pending") {
+        return; // 内部状態の変更は一度だけ
+      }
+      this.state = "fulfilled";
+      this.resolvedValue = resolvedValue;
+    };
+    const reject = (rejectedValue) => {
+      if (this.state !== "pending") {
+        return; // 内部状態の変更は一度だけ
+      }
+      this.state = "rejected";
+      this.rejectedValue = rejectedValue;
+    };
+
+    try {
+      func(resolve, reject);
+    }
+    catch (err) {
+      // 例外が発生したらrejectedにする
+      reject(err);
+    }
   }
 }
